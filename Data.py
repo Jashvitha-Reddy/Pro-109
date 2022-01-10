1f9eef3da231955dec79261d5bf9e4836ba83728
Jashvitha-Reddy <76088172+Jashvitha-Reddy@users.noreply.github.com>
2022-01-10 18:58:49 +0530

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,50 @@
+import csv
+import pandas as pd 
+import plotly.graph_objects as go 
+import plotly.figure_factory as ff
+import statistics 
+import random
+
+df=pd.read_csv("Performance.csv")
+data=df["reading score"].tolist()
+
+mean=sum(data)/len(data)
+median=statistics.median(data)
+mode=statistics.mode(data)
+std_deviation=statistics.stdev(data)
+
+first_std_deviation_start,first_std_deviation_end=mean-std_deviation,mean+std_deviation
+second_std_deviation_start,second_std_deviation_end=mean-(2*std_deviation),mean+(2*std_deviation)
+third_std_deviation_start,third_std_deviation_end=mean-(3*std_deviation),mean+(3*std_deviation)
+
+fig=ff.create_distplot([data],["reading score"],show_hist=False)
+fig=go.Scatter(x=[mean,mean],y=[0,0.17],mode="lines",name="MEAN")
+fig=go.Scatter(x=[first_std_deviation_start,first_std_deviation_start],y=[0,0.17],mode="lines",name="STANDARD DEVIATION 1")
+fig=go.Scatter(x=[first_std_deviation_end,first_std_deviation_end],y=[0,0.17],mode="lines",name="STANDARD DEVIATION 1")
+fig=go.Scatter(x=[second_std_deviation_start,second_std_deviation_start],y=[0,0.17],mode="lines",name="STANDARD DEVIATION 2")
+fig=go.Scatter(x=[second_std_deviation_end,second_std_deviation_end],y=[0,0.17],mode="lines",name="STANDARD DEVIATION 2")
+fig=go.Scatter(x=[third_std_deviation_start,third_std_deviation_start],y=[0,0.17],mode="lines",name="STANDARD DEVIATION 3")
+fig=go.Scatter(x=[third_std_deviation_end,third_std_deviation_end],y=[0,0.17],mode="lines",name="STANDARD DEVIATION 3")
+
+fig.show()
+
+list_of_data_within_1_std_deviation=[result for result in data if result>first_std_deviation_start and result<first_std_deviation_end]
+list_of_data_within_2_std_deviation=[result for result in data if result>second_std_deviation_start and result<second_std_deviation_end]
+list_of_data_within_3_std_deviation=[result for result in data if result>third_std_deviation_start and result<third_std_deviation_end]
+
+print("mean of this data is {}".format(mean))
+print("median of this data is {}".format(median))
+print("mode of this data is {}".format(mode))
+print("standard deviation of this data is {}".format(std_deviation))
+print("{}%  of data lies within 1 standard deviation".format(len(list_of_data_within_1_std_deviation)*100.0/len(dice_result)))
+print("{}%  of data lies within 2 standard deviation".format(len(list_of_data_within_2_std_deviation)*100.0/len(dice_result)))
+print("{}%  of data lies within 3 standard deviation".format(len(list_of_data_within_3_std_deviation)*100.0/len(dice_result)))
+
+
+
+
+
+
+
+
+
